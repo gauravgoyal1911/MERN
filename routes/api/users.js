94f71a768549e673bea387d187669437ddf75c7f
= <gauravgoyal1911@gmail.com>
2018-05-25 17:55:19 +0530

    Changes for User

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+//Load User model
+
+const User = require('../../models/User');
 
 //@route    GET api/users/test
 //@desc     Tests post route
@@ -9,5 +15,42 @@ router.get("/test", (req, res) =>
     msg: "Users Works"
   })
 );
+//@route    GET api/users/register
+//@desc     Register a User
+//@access   Public
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: 'Email already exists' });
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', //Size
+          r: 'pg', //Rating
+          d: 'nm' //Default
+        })
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar: avatar, //avatar
+          password: req.body.password
+        });
+        bcrypt.getSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err) => {
+            if (err) {
+              throw err;
+            }
+            newUser.password = hash;
+            newUser.save()
+              .then(user => {
+                res.json(user => res.json(user))
+                  .catch(err => console.log(err));
+              });
+          })
+        })
+      }
+    })
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
@@ -8,6 +10,12 @@ const app = express();
 
 //DB Config
 const db = require("./config/keys").mongoURI;
+
+//Body parser
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
 //Connect to MongoDB
 mongoose
